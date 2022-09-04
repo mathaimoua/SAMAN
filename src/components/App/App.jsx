d1258394a40df3f6b4c012cfb00d92828f74a993
Matt Moua <matmoua@gmail.com>
2022-09-04 13:59:15 -0500

    recent items table showing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -29,6 +29,7 @@ function App() {
 
   useEffect(() => {
     dispatch({ type: 'FETCH_USER' });
+    dispatch({type: 'FETCH_ITEMS' })
   }, [dispatch]);
 
   return (
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
+
   const user = useSelector((store) => store.user);
+  const items = useSelector((store) => store.items);
+
   return (
-    <div className="container">
+    <div className="dashBoard">
+    <div className="dashContainer">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <h2>Location</h2>
+      <button>View Items</button><button>Add New Item</button><button>Add New Container</button>
+      <h3>Recent Items:</h3>
+      <h1>{items.map(item => {
+        return (
+          <div>
+          <h5>{item.item_name} @ {item.container_name}</h5>
+          </div>
+        )
+      })}</h1>
+    </div>
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
