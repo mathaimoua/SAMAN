27e983628bb8d9f973ce7260d1b59145ffcfec18
Matt Moua <matmoua@gmail.com>
2022-09-11 14:59:30 -0500

    item details page displaying data

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,28 +1,36 @@
-import { useHistory, useParams } from 'react-router-dom'
-import { Paper, Box } from '@mui/material';
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { TextField } from '@mui/material'
+import { useHistory, useParams } from "react-router-dom";
+import {
+  Paper,
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField } from "@mui/material";
 import Moment from "react-moment";
-import moment from 'moment'
+import moment from "moment";
 
-function ItemDetails(){
-
-  const currentItem = useSelector(store => store.items.currentItem)
+function ItemDetails() {
+  const currentItem = useSelector((store) => store.items.currentItem);
   const dispatch = useDispatch();
   const itemID = useParams();
   const history = useHistory();
   const [fieldLocked, setFieldLocked] = useState(true);
-  const date = moment(currentItem.warranty_expiration)
-  console.log('date is', moment(date).format('MMM Do YYYY'))
+  const [itemName, setItemName] = useState("");
+
+  const handleItemNameChange = (event) => {
+    setItemName(event.target.value);
+  };
 
   useEffect(() => {
-    refresh(itemID.id)
+    refresh(itemID.id);
   }, [dispatch]);
 
   const refresh = () => {
-    dispatch({type: 'FETCH_CURRENT_ITEM', payload: itemID.id})
-  }
+    dispatch({ type: "FETCH_CURRENT_ITEM", payload: itemID.id });
+  };
 
   return (
     <div className="itemDetailsContainer">
@@ -34,24 +42,40 @@ function ItemDetails(){
       </button>
       <div className="itemDetailsDataContainer">
         <Box
-        sx={{textAlign: 'center', maxWidth: '100%', paddingLeft: '20px', paddingRight: '20px', boxShadow: 2}}
-        component={Paper}
+          sx={{
+            textAlign: "center",
+            maxWidth: "100%",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            paddingTop: '20px',
+            paddingBottom: '20px',
+            boxShadow: 2,
+          }}
+          component={Paper}
         >
-          <h2 className='itemDetailsHeader'>Item Details for {currentItem.item_name}</h2>
-          Name:
-          <TextField
-           disabled={fieldLocked}
-           size='small'
-           sx={{margin: '5px'}}
-           id="standard-disabled"
-           label="Name"
-           defaultValue={currentItem.item_name}
-          />
-          <br></br>
+
+            <span style={{display: 'inline'}}>
+              Name: </span><h2 style={{display: 'inline'}}>{currentItem.item_name}</h2>
+            <br></br>
+            <span style={{display: 'inline'}}>
+              Holder: </span><h2 style={{display: 'inline'}}>{currentItem.current_holder}</h2>
+            <br style={{margin: '10px'}}></br>
+            <span style={{display: 'inline'}}>
+              Container: </span><h2 style={{display: 'inline'}}>{currentItem.container_name}</h2>
+            <br></br>
+            <span style={{display: 'inline'}}>
+              Model: </span><h2 style={{display: 'inline'}}>{currentItem.model}</h2>
+            <br></br>
+            <span style={{display: 'inline'}}>
+              Serial: </span><h2 style={{margin: '10px', display: 'inline'}}>{currentItem.serial}</h2>
+            <br></br>
+            <span style={{display: 'inline'}}>
+              Warranty Expiration: </span><h2 style={{display: 'inline'}}>{moment(currentItem.warranty_expiration).format('MMM Do YYYY')}</h2>
+            
         </Box>
       </div>
     </div>
-  )
+  );
 }
 
 export default ItemDetails;
